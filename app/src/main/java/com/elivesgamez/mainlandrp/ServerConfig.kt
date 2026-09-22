ac77f9370adfa51cb7fc7098f84a922098657e55
Elives <elives@mainlandrp.com>
2026-09-22 11:42:53 -0700

    Link real server IP/port and Alyn game client package

--- app/src/main/java/com/elivesgamez/mainlandrp/ServerConfig.kt
+++ app/src/main/java/com/elivesgamez/mainlandrp/ServerConfig.kt
@@ -7,8 +7,6 @@ object ServerConfig {
     const val HOST = "188.127.241.74"
     const val PORT = 3836
 
-    // Package name of the installed GTA:SA + SA-MP mobile client that will
-    // actually render the game. TODO: fill in once we know which client
-    // your players use (Phase 2 will attempt to launch it with this info).
-    const val GAME_CLIENT_PACKAGE = ""
+    // Alyn SA-MP Mobile - the launcher engine Mainland RP's client runs on.
+    const val GAME_CLIENT_PACKAGE = "ro.alynsampmobile.launcher"
 }
