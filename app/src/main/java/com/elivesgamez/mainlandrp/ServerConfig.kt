a4721cbae3949892b614e37f6ba14aa3f66d0e9f
Elives <elives@mainlandrp.com>
2026-09-22 01:58:17 -0700

    Set real server IP and port

--- app/src/main/java/com/elivesgamez/mainlandrp/ServerConfig.kt
+++ app/src/main/java/com/elivesgamez/mainlandrp/ServerConfig.kt
@@ -4,8 +4,8 @@ package com.elivesgamez.mainlandrp
  * EDIT THESE for your real Mainland RP server.
  */
 object ServerConfig {
-    const val HOST = "play.mainlandrp.com" // TODO: replace with real IP or domain
-    const val PORT = 7777                  // TODO: replace with real SA-MP port
+    const val HOST = "188.127.241.74"
+    const val PORT = 3836
 
     // Package name of the installed GTA:SA + SA-MP mobile client that will
     // actually render the game. TODO: fill in once we know which client
